78daa3bcf5bd5b9d1af044facbed302ef720bf86
Erik Castricum <git@cas-online.nl>
2021-10-05 14:14:04 +0200

    fix the script, add errorhandler
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- deleted.py
+++ deleted.py
@@ -1,9 +1,14 @@
 import json
 import tweepy
+import sys
 from tweepy import Stream
 from tweepy import StreamListener
 from post import post_deleted_tweet
 
+log = open('log_deleted.log', 'a')
+sys.stdout = log
+
+print('Let the game begin')
 # Get the twitter credentials from a (hidden) file
 secrets = open(".login")
 login = secrets.readlines()
@@ -24,7 +29,7 @@ auth.set_access_token(access_token, access_token_secret)
 # calling the api
 api = tweepy.API(auth)
 
-class CassieListener(StreamListener):
+class DeletedListener(StreamListener):
     
     def on_data(self, data):
             
@@ -33,26 +38,30 @@ class CassieListener(StreamListener):
         #Filter alleen de tweets door Kaffie eruit
         try:
             if json_data['user']['id_str'] == '1347228691819077632':
+                print('Voila, een tweet van onze praatzanger?')
                 text_file = open(json_data['id_str']+'_Kaffie.json', 'a')
                 #json.dump(data, text_file, indent=4)
                 text_file.write(data)
                 text_file.close()
-                
-            if 'delete' in data:
-                if json_data['delete']['status']['user_id'] == '1347228691819077632':
-                    print(' Ah een gedelete tweet jongens')
-                    text_file = open(json_data['delete']['status']['id_str']+'_deleted_Kaffie.json', 'a')
-                    #json.dump(data, text_file, indent=4)
-                    text_file.write(data)
-                    text_file.close()
-                    # Post de tweet naar een wordpress
-                    tweet_id = json_data['delete']['status']['id_str']
-                    post_deleted_tweet(tweet_id)
         except KeyError:
-            print("Oh jee, een error. We skippen deze")
+            print('Misschien een deleted tweet?')
+
+        try:
+            if 'delete' in data:
+                print(' Ah een gedelete tweet jongens')
+                text_file = open(json_data['delete']['status']['id_str']+'_deleted_Kaffie.json', 'a')
+                #json.dump(data, text_file, indent=4)
+                text_file.write(data)
+                text_file.close()
+                # Post de tweet naar een wordpress
+                print('Posten maar')
+                tweet_id = json_data['delete']['status']['id_str']
+                post_deleted_tweet(tweet_id)
+        except KeyError:
+            print("Och nee toch, een error. We skippen deze")
             
         return True
 
-listener = CassieListener()
+listener = DeletedListener()
 twitterStream = Stream(auth, listener)
 twitterStream.filter(follow=['1347228691819077632'])
