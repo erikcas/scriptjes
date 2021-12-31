7b51195480861f2582d5c92e82ee71c61050fc17
Erik Castricum <git@cas-online.nl>
2021-12-31 11:33:08 +0100

    capture: improve capture proces
    * add more waiting time for chromedriver to load data
    * kill chromium and chromedriver after capture, as they are not always
      closed correctly by the module
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- capture.py
+++ capture.py
@@ -2,7 +2,8 @@ from tweetcapture import TweetCapture
 import asyncio
 import logging
 from selenium.common.exceptions import WebDriverException
-
+import time
+import os
 logging.basicConfig(filename='log_deleted.log',
         filemode = 'a',
         format='%(asctime)s %(levelname)-8s %(message)s',
@@ -11,11 +12,20 @@ logging.basicConfig(filename='log_deleted.log',
 
 tweet = TweetCapture()
 
+cp = '<path-to-chromedriver>'
+chromium = '<path/to/chromium-browser>'
 def tweet_screenshot(tweet_id, scherm_naam):
     url = f'https://twitter.com/{scherm_naam}/status/{tweet_id}'
     print(url)
     output = f'screenshots/{scherm_naam}_{tweet_id}.png'
     try:
+        tweet.set_chromedriver_path(cp)
+        tweet.set_wait_time(7)
         asyncio.run(tweet.screenshot(url, output, mode=2, night_mode=0))
     except WebDriverException as e:
         logging.debug(f'no screenshot made. Error {e}')
+        #print(f'oeps {e}')
+    # Sleep 5 seconds and kill chromedriver and chrome
+    logging.debug('[SCREENSHOT]Sleep 5 seconds and kill chromedriver and chrome')
+    time.sleep(5)
+    os.system('/usr/bin/killall <path-to-chromedriver> && /usr/bin/killall chromium)
